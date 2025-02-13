95c9fe53f3e378b09f82c3985d7fc8fb8e83be1f
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-02-13 09:07:59 +0100

    Apply suggestions from code review

--- packages/auto-sync/ui/src/main.tsx
+++ packages/auto-sync/ui/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "@db-ux/core-components/build/styles/db-ux-42-rollup.css";
+import "@db-ux/core-components/build/styles/rollup.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
